b4c6a733c84a101963ea5801615b888f2b6c7536
piyushk46 <132258590+piyushk46@users.noreply.github.com>
2024-10-20 11:51:25 +0530

    Update hello.cpp
    updated hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main()
 {
   cout<<"Hello, From Piyush<<endl;
+  string name;
+  cout<<"Enter your first name"<<endl;
+  cin>>name;
+  cout<<"Hello, from "<<name<<endl;
   return 0;
 }
